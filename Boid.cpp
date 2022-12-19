66c4e22a9990d81969a7e59ada52e5c6fa8d324d
Francesco~ <fra.santo00@gmail.com~>
2022-12-19 10:31:11 +0100

    Ho aggiunto gli ultimi if di separation in boid

--- Boid.cpp
+++ Boid.cpp
@@ -1,14 +1,14 @@
 #include "Boid.h"
 
-//la funzione si applica ad un boid e scorre tutti gli altri
-//misurandone la distanza. Se il boid è dentro al raggio d'azione
-//allora creo un "Vettore media" che indica la nuova direzione e intensità
-//della velocità. Ritorno il vettore media.
-//Per farlo normalizziamo la differenza tra le posizioni e la moltiplichiamo
-//per maxvelocity considerando un peso (maggiore se sei più vicino).
-//Le singole variazioni sono sommate e divise infine per il numero di
-//boid nel raggio (count).
-BVector Boid::separation(const std::vector<Boid> boids) 
+// la funzione si applica ad un boid e scorre tutti gli altri
+// misurandone la distanza. Se il boid è dentro al raggio d'azione
+// allora creo un "Vettore media" che indica la nuova direzione e intensità
+// della velocità. Ritorno il vettore media.
+// Per farlo normalizziamo la differenza tra le posizioni e la moltiplichiamo
+// per maxvelocity considerando un peso (maggiore se sei più vicino).
+// Le singole variazioni sono sommate e divise infine per il numero di
+// boid nel raggio (count).
+BVector Boid::separation(const std::vector<Boid> boids)
 {
   float count = 0;
   BVector sum;
@@ -18,25 +18,36 @@ BVector Boid::separation(const std::vector<Boid> boids)
     if (d > 0 && d < SepRange)
     {
       BVector dev;
-      dev.setvalue(0,0);
+      dev.setvalue(0, 0);
       dev.vectorTwoSub(position, boids[i].position);
       dev.normalize();
-      dev.scalarMul(1-d/SepRange);
+      dev.scalarMul(1 - (d / SepRange));
       dev.scalarMul(maxVel);
       count++;
       sum.vectorSum(dev);
     }
   }
-  sum.scalarDiv(count);
+  if (count > 0)
+  {
+    sum.scalarDiv((float)count);
+  }
+
+  if (sum.norm() > 0)
+  {
+    // Steering = sum - Velocity
+    sum.normalize();
+    sum.scalarMul(maxVel);
+    sum.vectorSub(velocity);
+    sum.limitsize(maxAcc);
+  }
   return sum;
 }
 
-
-//Si applica ad un boid e ritorna lo "steer".
-//Creo target e lo sottraggo a un vettore in ingresso, così
-//che diventi il negativo del punto verso cui tendo. Normalizzo,
-//moltiplico per la velocità massima (così diventa velocità). 
-//sottraggo alla velocità del boid il nuovo vettore, e ne limito la dimensione.
+// Si applica ad un boid e ritorna lo "steer".
+// Creo target(0,0) e lo sottraggo a un vettore in ingresso, così
+// che diventi il negativo del punto verso cui tendo. Normalizzo,
+// moltiplico per la velocità massima (così diventa target diventa velocità).
+// sottraggo alla velocità del boid il vettore target, e ne limito la dimensione.
 BVector Boid::seek(BVector v)
 { // v è il target
   BVector target;
--- main.cpp
+++ main.cpp
@@ -5,14 +5,6 @@ using namespace std;
 
 int main ()
 {
-    BVector v1(2,5);
-    BVector v2;
-    Boid b(v1);
-    std::cout << b.acceleration.x << endl;
-    std::cout << b.velocity.x << endl;
-    std::cout << b.position.x << endl;
-    std::cout << endl;
-    std::cout << b.acceleration.y << endl;
-    std::cout << b.velocity.y << endl;
-    std::cout << b.position.y << endl;
+
+
 }
